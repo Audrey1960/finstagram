e45af8f0ee2e8ca173828956cc57c8910c30357c
abrouillette <abrouillette@labplas.com>
2019-10-24 19:55:40 -0400

    FinstagramV6

--- app/actions.rb
+++ app/actions.rb
@@ -1,16 +1,29 @@
 get '/' do
-    @finstagram_posts = FinstagramPost.order(created_at: :desc)
-    erb(:index)
+  @finstagram_posts = FinstagramPost.order(created_at: :desc)
+  erb(:index)
 end
 
-get '/signup' do      # if a user navigates to the path "/signup",
-  @user = User.new    # setup empty @user objet
-  erb(:signup)        # render "app/views/signup.erb"
-  
+get '/signup' do     # if a user navigates to the path "/signup",
+  @user = User.new   # setup empty @user object
+  erb(:signup)       # render "app/views/signup.erb"
 end
+
+# post '/signup' do
+#   params.to_s
+# end
+
 
 post '/signup' do
-  params.to_s
+  email      = params[:email]
+  avatar_url = params[:avatar_url]
+  username   = params[:username]
+  password   = params[:password]
+
+  @user = User.new({ email: email, avatar_url: avatar_url, username: username, password: password })
+
+  if @user.save
+    "User #{username} saved!"
+  else
+    erb(:signup)
+  end
 end
-
-
